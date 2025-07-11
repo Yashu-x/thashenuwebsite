72f5b2fac952aa9b679f8aee6c0c874e3e156b88
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-07-11 14:23:35 +0530

    idk

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,5 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    
-  },
-}
-
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
 
 export default config;
